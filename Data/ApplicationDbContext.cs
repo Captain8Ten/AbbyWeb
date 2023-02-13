c0e0d07fe6df9da3db3559ee56ecb546e16ba205
VAN1LLA <tlink1776@gmail.com>
2023-02-13 14:58:23 -0500

    Continued development

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,6 +5,11 @@ namespace AbbyWeb.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
         public DbSet<Category> Category { get; set; }
     }
 }
